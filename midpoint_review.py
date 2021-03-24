87ecf09d0728d407a391423739c3988c155c4911
jebbica <jessicalin4903@gmail.com>
2021-03-23 17:58:25 -0700

    one done woo

---
+++ midpoint_review.py
@@ -0,0 +1,11 @@
+# Jessica Lin
+'''
+1.
+Create Input Variables for your first name, last name and age.
+Then print out your full name and age within a sentence -> My name is Hunter Macias & 
+I am 20 years old
+'''
+first_name = input("What is your first name? ")
+last_name = input("What's your last name? ")
+age = input("Enter your age. ")
+print(f"My name is {first_name} {last_name} & I am {age} years old")
