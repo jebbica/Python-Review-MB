259d3845361c937c9353e5c13cfcf41368944a9f
jebbica <jessicalin4903@gmail.com>
2021-03-23 18:02:42 -0700

    two do'd

--- midpoint_review.py
+++ midpoint_review.py
@@ -5,7 +5,21 @@ Create Input Variables for your first name, last name and age.
 Then print out your full name and age within a sentence -> My name is Hunter Macias & 
 I am 20 years old
 '''
-first_name = input("What is your first name? ")
-last_name = input("What's your last name? ")
-age = input("Enter your age. ")
-print(f"My name is {first_name} {last_name} & I am {age} years old")
+# first_name = input("What is your first name? ")
+# last_name = input("What's your last name? ")
+# age = input("Enter your age. ")
+# print(f"My name is {first_name} {last_name} & I am {age} years old.")
+
+'''
+2. 
+create 3 different variables with any whole number assined as the value and convert them into a string using the str() constructor.
+once you convert them print out the variables. Example: convert 3 to '3'
+if you use print(type(<var_name>)) this will print out the type of variable for example x = 3 would print 'int' and x = '3' would print 'str'
+'''
+num1 = str(3243)
+num2 = str(4849)
+num3 = str(42069)
+
+print(num1, type(num1))
+print(num2)
+print(num3)
